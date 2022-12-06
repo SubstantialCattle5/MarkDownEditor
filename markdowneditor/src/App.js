d9d1033358a7d407da09404860392f2b2e176ce7
nilay <nilaynathsharan16@gmail.com>
2022-12-06 17:52:36 +0530

    feat: adding file structure

--- markdowneditor/src/App.js
+++ markdowneditor/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./navbar/Navbar";
+import Display from "./body/display/Display";
+import Editor from "./body/editor/Editor";
+import Footer from "./footer/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Display />
+      <Editor />
+      <Footer />
+    </>
   );
 }
-
 export default App;
